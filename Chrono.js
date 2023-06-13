4bf72903c738681f31f11dbda6311b4e6f32978b
Doudou210 <siakakoudou.doumbia@yahoo.com>
2023-06-13 17:23:54 +0200

    update and edit js

--- Chrono.js
+++ Chrono.js
@@ -1,3 +1,5 @@
+//Chronomètre
+
 let $time = document.querySelector('.timer');
 let $start = document.querySelector('.start');
 let $split = document.querySelector('.split');
@@ -7,6 +9,7 @@ let $secs = document.querySelector('#secondes');
 let $milli = document.querySelector('#milliSecondes');
 let $min = document.querySelector('#minutes');
 let $body =document.querySelector('.body');
+let $result =document.getElementById('#resultTable');
 
 let millisecondes = 0;
 let secondes = 0;
@@ -43,16 +46,19 @@ $start.addEventListener('click',function(){
 
         if(minutes>59){
             minutes = 0;
-            $min.innerHTML = minutes;   
+            $min.innerHTML = minutes;
         }
         
     },100);
+
+    // Pour desactiver certains button
 
     $start.setAttribute('disabled', '');
     $reset.setAttribute('disabled', '')
     $stop.removeAttribute('disabled');
     $split.removeAttribute('disabled')
 
+    //Ajouter une class au button
     $start.classList.add('active');
     $stop.classList.remove('active');
     $split.classList.remove('active');
@@ -61,27 +67,34 @@ $start.addEventListener('click',function(){
 
 $stop.addEventListener('click', function(){
     clearInterval(interval);
+    
+    // Pour desactiver certains button
+    
     $stop.setAttribute('disabled', '');
     $start.removeAttribute('disabled');
     $reset.removeAttribute('disabled');
     $split.setAttribute('disabled','')
+    
+    //Ajouter et rétirer une class au button
 
     $stop.classList.add('active');
     $start.classList.remove('active');
     $split.classList.add('active');
     $reset.classList.remove('active');
-
+    
 });
 
 $reset.addEventListener('click', function(){
-    millisecondes = 0;
-    $milli.innerHTML= '0' + millisecondes;
+    minutes =0 ;
+    $min.innerHTML= '0' + minutes;
 
     secondes = 0;
     $secs.innerHTML= '0' + secondes;
-
-    minutes =0 ;
-    $min.innerHTML= '0' + minutes;
+    
+    millisecondes = 0;
+    $milli.innerHTML= '0' + millisecondes;
+    
+    //Ajouter et rétirer une class au button
 
     $reset.classList.add('active');   
     $split.classList.add('active');   
@@ -95,30 +108,75 @@ $reset.addEventListener('click', function(){
     $body.innerHTML='';
 })
 
-//Afficher les selections du buttons
-
 $split.addEventListener('click', function(){
     $body.innerHTML+= `<p>${minutes}: ${secondes}. ${millisecondes}</p>`
 })
 
+//Afficher les selections du buttons
+
 let $affBtn = document.querySelectorAll('.afficheur button');
 let $content = document.querySelectorAll('.contents .content');
 
-let $active = document.querySelector('.afficheur button.is-active');
-let $activeContent = document.querySelector('.contents .content.is-actived');
 
 for(let i=0; i<$affBtn.length; i++){
-
+    
     $affBtn[i].addEventListener('click',()=>{
+        let $active = document.querySelector('.afficheur button.is-active');
+        let $activeContent = document.querySelector('.contents .content.is-actived');
         
-        // $active.classList.remove('is-active');
-        $active.removeAttribute("class");
-        // $active.classList.add('actived');
-        $activeContent.classList.remove('is-actived');
+        $active.classList.toggle('is-active');
+        $activeContent.classList.toggle('is-actived');
         console.log(i);
         
-        $content[i].classList.add('is-actived');
+        $content[i].classList.toggle('is-actived');
         $affBtn[i].classList.add('is-active');
 
     })
 }
+////////////////////////////////////////////////////////////
+
+//Alarme
+
+
+
+////////////////////////////////////////////////////////////
+
+// Minueur
+
+
+
+////////////////////////////////////////////////////////////
+
+//Horloge
+
+$timer = document.querySelector('time');
+var update = ()=>{
+
+    var date = new Date();
+
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+
+    if (h<10)
+    {
+        h= '0' + h;
+    }
+
+    if (m <10)
+    {
+        s= '0' + s;
+    }
+    
+    if(s < 10)
+    {
+        s= '0' + s;
+    }
+
+    var time = h + ':' + m + ':' + s;
+    
+    $timer.innerText = time;
+};
+
+update();
+setInterval(update, 1000);
